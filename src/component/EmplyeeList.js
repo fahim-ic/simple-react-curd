bd4e05837c1e06802872c21272fe5b07b996fbd4
Farhadur Reja Fahim <fahim@interconnectionbd.com>
2019-07-31 19:31:43 +0600

    Change list key by id

--- src/component/EmplyeeList.js
+++ src/component/EmplyeeList.js
@@ -5,13 +5,8 @@ class EmployeeList extends Component {
     constructor(props) {
         super(props);
         this.keyCount = 0
-        this.getKey = this.getKey.bind(this)
         this.handleEdit = this.handleEdit.bind(this)
         this.handleDelete = this.handleDelete.bind(this)
-    }
-
-    getKey() {
-        return this.keyCount++
     }
 
     handleEdit(index) {
@@ -33,7 +28,7 @@ class EmployeeList extends Component {
                 <ul className="list-group">
                 {employeeList.map( (item, index) => {
                     return (
-                        <li className="list-group-item" key={this.getKey()}>
+                        <li className="list-group-item" key={item.id}>
                             Name: {item.name}, Age: {item.age}
                             <button type="button" className="btn btn-primary" onClick={ () => this.handleEdit(index) }>Edit</button>
                             <button type="button" className="btn btn-danger" onClick={ () => this.handleDelete(index) }>Delete</button>
